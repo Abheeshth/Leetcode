ed7eef9f61e6605fe4e3657005bf78020e77b0ed
ABHEESHTH  MISHRA <59581245+Abheeshth@users.noreply.github.com>
2022-03-11 16:27:29 +0530

    Time: 32 ms (95.67%), Space: 14.3 MB (39.46%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,27 +1,18 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # let's write a recurssion function
-        self.final = []
-        def function(temp,left,right,n):
-            print(left,right)
-            if left+right == 2*n and left == right:
-                print('1')
-                self.final.append(temp)
-
-            if left>right and left<n and right<n:
-                print('2')
-                function(temp+'(',left+1,right,n)
-                function(temp+')',left,right+1,n)
-            if left>right and left == n and right<n:
-                function(temp+')',left,right+1,n)
-                
-    
-            if left == right and left<n and right<n:
-                print('3')
-                function(temp+'(',left+1,right,n)
- 
-        function("",0,0,n)
-        return self.final
+        self.l = []
+        def dfs(right,left,stri):
+            if right<0 or left<0:
+                return
+            if right > left:
+                return
+            if right == 0 and left == 0:
+                self.l.append(stri)
+                return 
+            dfs(right-1,left,stri+'(')
+            dfs(right,left-1,stri+')')
+        dfs(n,n,'')
+        return self.l
                 
                 
             
